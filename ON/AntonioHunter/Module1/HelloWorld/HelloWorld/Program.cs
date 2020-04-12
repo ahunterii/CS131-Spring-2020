074795fb068256ea3e8ab4457f48435eb9cab2a6
Antonio Hunter <hunterantonio@cityuniversity.edu>
2020-04-12 16:22:59 -0400

    HOP01

---
+++ ON/AntonioHunter/Module1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Antonio";
+            int Age = 29;
+            bool isFemale = true;
+            double myfloat = 10.1;
+
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);
+            Console.WriteLine(isFemale);
+            Console.WriteLine(myfloat);
+          
+        }
+    }
+}
